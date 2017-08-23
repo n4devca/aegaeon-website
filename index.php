e5850c783b54ffd1510266c3ac60c3ba2cf61747
Remi Guillemette <rguillemette@n4dev.ca>
2017-08-23 17:05:41 -0400

    More styling and better images

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en-CA" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <title>Aegaeon - Open Source OpenID server</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="index, follow">
+
+    <link href="assets/css/normalize.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+
+    <!-- <link rel="shortcut icon" href="https://developer.cdn.mozilla.net/static/img/favicon32.e1ca6d9bb933.png"> -->
+    <meta name="description" content="Aegaeon is an openid / oauth 2.0 server.">
+
+    <script type="text/javascript">
+
+    </script>
+
+</head>
+<body>
+    <header>
+        <div class="header-inner">
+            <div class="header-image">
+
+            </div>
+        </div>
+    </header>
+    <div class="container">
+        <div class="container-sections">
+
+            <section id="section-what" class="section-text">
+                <h1>What is it?</h1>
+                <div>
+                    <p>
+                        Aegaeon is an OpenID Connect / OAuth 2.0 server implementation using spring boot. The project was originally inspired by mitreid-connect.
+                    </p>
+                    <p>
+                        The code is available under Apache 2.0 license and is currently under heavy development.
+                    </p>
+                    <p>
+                        The project was started with the goal of having a better understanding of OpenId / OAuth standard and has evolved to a project with the following goal :
+                        <ul>
+                            <li>Creating an <i>opiniatre</i> implementation. i.e. Having sensible default and being pragmatic.</li>
+                            <li>Simple, fast and clean</li>
+                            <li>Being up-to-date</li>
+                        </ul>
+                    </p>
+                    <p>
+                        <h2>Features (completed and planned)</h2>
+                        <ul>
+                            <li>Implicit flow (completed)</li>
+                            <li>Authorization Code (completed)</li>
+                            <li>Client Credential (ongoing)</li>
+                            <li>Full error handling (ongoing)</li>
+                            <li>User info endpoint (completed)</li>
+                            <li>Introspect endpoint (completed)</li>
+                            <li>JWT using RSA signature (completed)</li>
+                            <li>JWT using HMAC Protection (completed)</li>
+                            <li>Clustering ready (ongoing)</li>
+                            <li>Basic Client administration</li>
+                            <li>API Scopes / Claims centric</li>
+                        </ul>
+                        <h2>Notable Technologies</h2>
+                        <ul>
+                            <li>Spring Framework 4.3.8</li>
+                            <li>Spring Boot 1.5.3</li>
+                            <li>Hibernate 5.0.12</li>
+                            <li>Thymeleaf 3.0.5</li>
+                            <li>Nimbus 4.23</li>
+                        </ul>
+                    </p>
+                </div>
+
+            </section>
+
+            <section id="section-download" class="section-text">
+                <h1>Download</h1>
+                <div>
+                    <p>To download the latest version, follow the link below :</p>
+                    <p>
+                        <a href="https://github.com/n4devca/aegaeon" target="_blank">https://github.com/n4devca/aegaeon</a>
+                    </p>
+                </div>
+            </section>
+            <section id="section-source" class="section-text">
+                <h1>Sources</h1>
+                <div>
+                    <p>Aegaeon is fully open source and is available under the Apache 2.0 license.</p>
+                    <p>You may download the source on our Github repository at the following address :</p>
+                    <p>
+                        <a href="https://github.com/n4devca/aegaeon" target="_blank">https://github.com/n4devca/aegaeon</a>
+                    </p>
+                    <p>Fork if you need and pull requests are welcome!</p>
+                </div>
+            </section>
+
+            <section id="section-doc" class="section-text">
+                <h1>Documentation</h1>
+                <div>
+                <p>
+                    Soon...
+                </p>
+                </div>
+            </section>
+
+            <section id="section-contact" class="section-text">
+                <h1>Contact Us</h1>
+                <div>
+                <p>If you have discovered a bug, please open an issue on Github with as much details as possible.</p>
+                <p>You can also reach us at the following email address : support AT aegaeon.ca</p>
+                </div>
+            </section>
+        </div>
+        <div class="container-menu">
+            <div class="container-menu-links">
+                <a href="#section-what" class="menu-list-item">What is it?</a>
+                <a href="#section-download" class="menu-list-item">Download</a>
+                <a href="#section-source" class="menu-list-item">Get the source</a>
+                <a href="#section-doc" class="menu-list-item">Documentation</a>
+                <a href="#section-contact" class="menu-list-item">Contact Us</a>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
